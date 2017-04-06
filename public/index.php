ff3cb13b89f6f926658cc1b7b353a07d2a0cda60
Carter Fort <carter.fort@gmail.com>
2017-04-06 16:40:15 -0400

    Implementing basic UI using the GET variable to fetch the string

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
 require __DIR__ . "./../vendor/autoload.php";
+
+$string = $_GET["string"] ?? "";
+
+if ( $string == ''){
+	dd("Use a get variable named 'string' to convert it: /?string=IT CROWD ");
+}
+
+$converter = new App\ConvertsString();
+
+$results = collect($converter->convert($string));
+
+dd($results->implode(','));
