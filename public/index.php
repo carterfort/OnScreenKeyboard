e28a68f5ec7db37968bf64613035c6d73e1b1078
Carter Fort <carter.fort@gmail.com>
2017-04-07 10:51:10 -0400

    Added visual keyboard layout. Adding controls

--- public/index.php
+++ public/index.php
@@ -7,20 +7,55 @@
 </head>
 <body>
 
-	<section class="section">
+	<section class="section" id="television">
 		<div class="container">
 			<div class="columns">
 				<div class="column is-8">
 					<div class="card">
 						<div class="card-content">
-						Keyboard goes here
+							<div class="columns" v-for="row in keyboardRows">
+								<div class="column" v-for="key in row">
+									<div class="box has-text-centered" :class="{ 'is-active' : keyIsActive(key), 'is-pressed' : keyIsPressed(key) }">
+										{{ key.key }}
+									</div>
+								</div>
+							</div>
+
+							<div class="box has-text-centered" :class="{ 'is-pressed' : spaceBarPressed }">
+								Space
+							</div>
 						</div>
 					</div>
 				</div>
 				<div class="column is-4">
 					<div class="card">
 						<div class="card-content">
-						Remote goes here
+
+							<div class="columns">
+								<div class="column is-4 is-offset-4">
+									<button class="button is-block" @click="moveCursor(0)">Up</button>
+								</div>
+							</div>
+
+							<div class="columns">
+								<div class="column is-4">
+									<button class="button is-block" @click="moveCursor(3)">Left</button>
+								</div>
+								<div class="column is-4">
+									<button class="button is-block" @click="pressKey()">Go!</button>
+								</div>
+								<div class="column is-4">
+									<button class="button is-block" @click="moveCursor(1)">Right</button>
+								</div>
+							</div>
+
+							<div class="columns">
+								<div class="column is-4 is-offset-4">
+									<button class="button is-block" @click="moveCursor(2)">Down</button>
+								</div>
+							</div>
+
+							
 						</div>
 					</div>
 				</div>
@@ -28,6 +63,6 @@
 		</div>
 	</section>
 
-	<script src="/js/app.js"></script>
+	<script src="/assets/app.js"></script>
 </body>
 </html>
