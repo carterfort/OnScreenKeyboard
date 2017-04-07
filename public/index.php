c056cf6d5f6e00aae6455789d4afd7ccac7f5f1a
Carter Fort <carter.fort@gmail.com>
2017-04-07 08:46:41 -0400

    Started initial work on UI

--- public/index.php
+++ public/index.php
@@ -1,15 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TrueFit Remote</title>
 
-require __DIR__ . "./../vendor/autoload.php";
+	<link rel="stylesheet" href="/assets/app.css" />
+</head>
+<body>
 
-$string = $_GET["string"] ?? "";
+	<section class="section">
+		<div class="container">
+			<div class="columns">
+				<div class="column is-8">
+					<div class="card">
+						<div class="card-content">
+						Keyboard goes here
+						</div>
+					</div>
+				</div>
+				<div class="column is-4">
+					<div class="card">
+						<div class="card-content">
+						Remote goes here
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-if ( $string == ''){
-	dd("Use a get variable named 'string' to convert it: /?string=IT CROWD ");
-}
-
-$converter = new App\ConvertsString();
-
-$results = collect($converter->convert($string));
-
-dd($results->implode(','));
+	<script src="/js/app.js"></script>
+</body>
+</html>
